a8826aa93bde079386e4062455c04c75196b39a6
harijo123 <hariijack.97@gmail.com>
2023-06-06 13:57:44 +0530

    jo

--- src/org/launch/Same1.java
+++ src/org/launch/Same1.java
@@ -1,14 +1,13 @@
 package org.launch;
 
-public abstract class Same1 {
+public  class Same1 {
 
 	public void method1() {
-System.out.println("hari");
+System.out.println("praveen");
 	}
 	
-	public abstract void method2();
-//	public static void main(String[] args) {
-//		Same1 s = new Same1();
-//		s.method1();
-//	}
+	public static void main(String[] args) {
+		Same1 s = new Same1();
+		s.method1();
+	}
 }
