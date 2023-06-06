490453da831e99878c8e2d161f493c47bd4b8227
harijo123 <hariijack.97@gmail.com>
2023-06-06 21:43:07 +0530

    sathy

--- src/org/launch/Same1.java
+++ src/org/launch/Same1.java
@@ -1,11 +1,14 @@
 package org.launch;
 
-public  class Same1 {
+public class Same1 {
 
 	public void method1() {
-System.out.println("praveen");
+		System.out.println("mayiladuthurai");
 	}
-	
+	public void employee() {
+		System.out.println("harijo");
+	}
+
 	public static void main(String[] args) {
 		Same1 s = new Same1();
 		s.method1();
